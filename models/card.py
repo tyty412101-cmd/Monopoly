afa3359b9907cd288fc85771b5961092d9963b31
tyty412101-cmd <tyty412101@gmail.com>
2026-04-20 15:18:23 -0400

    Delete models directory

--- models/card.py
+++
@@ -1,8 +0,0 @@
-class Card:
-    def __init__(self, description, action):
-        self.description = description
-        self.action = action
-
-    def apply(self, player, game):
-        print(self.description)
-        self.action(player, game)
--- models/utility.py
+++
@@ -1,5 +0,0 @@
-from models.property import Property
-
-class Utility(Property):
-    def __init__(self, name):
-        super().__init__(name, price=150, rent=20)
